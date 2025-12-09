273220d64ce08b1fee083dd3ac4ec2ba848aa92e
Ilmari Kylliäinen <ilmari.kylliainen@front.ai>
2025-12-09 17:06:28 +0200

    Update version code to 3 and version name to 1.2 in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.ilmariware.currencyconverterwidget"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
