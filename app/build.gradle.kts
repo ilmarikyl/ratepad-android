3e03e62278f2af24086c241e9cbefd2c6322f85b
Ilmari Kylliäinen <ilmari.kylliainen@front.ai>
2026-03-26 20:14:33 +0200

    Update version code to 7 and version name to 1.6 in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.ilmariware.currencyconverterwidget"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.5"
+        versionCode = 7
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
